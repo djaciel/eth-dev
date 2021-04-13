8562336617dd3b3d926d886b2cc5390b215c3855
djaciel <dj_roses@hotmail.com>
2021-04-13 00:31:58 -0500

    addresses and balances

--- course/Variables.sol
+++ course/Variables.sol
@@ -12,4 +12,24 @@ contract WorkingWithVariables {
     function setMyBool(bool _myBool) public {
         myBool = _myBool;
     }
+
+    uint8 public myUint8;
+    
+    function incrementUint() public {
+        myUint8++;
+    }
+    
+    function decrementUint() public {
+        myUint8--;
+    }
+    
+    address public myAddress;
+    
+    function setAddress (address _address) public {
+        myAddress = _address;
+    }
+    
+    function getBalanceOfAddress() public view returns(uint) {
+        return myAddress.balance;
+    }
 }
