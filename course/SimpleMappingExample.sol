9e3666b4ae8db94982c5dfe83a0694a9beeea39b
djaciel <dj_roses@hotmail.com>
2021-04-13 23:52:34 -0500

    simple mapping example

---
+++ course/SimpleMappingExample.sol
@@ -0,0 +1,15 @@
+pragma solidity ^0.8.3;
+
+contract SimpleMappingExample {
+    
+    mapping(uint => bool) public myMapping;
+    mapping(address => bool) public myAddressMapping;
+    
+    function setValue(uint _index) public {
+        myMapping[_index] = true;
+    }
+    
+    function setMyAddressToTrue() public {
+        myAddressMapping[msg.sender] = true;
+    }
+}
