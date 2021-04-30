45fcc64f005afb5bac0e511b92b867f5538bf3ad
djaciel <dj_roses@hotmail.com>
2021-04-30 00:09:41 -0500

    improve smart contract structure

--- course/SimpleWallet/SimpleWallet.sol
+++ course/SimpleWallet/SimpleWallet.sol
@@ -2,12 +2,15 @@ pragma solidity ^0.8.3;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
-contract SimpleWallet is Ownable {
-    
+contract Allowance is Ownable {
     mapping(address => uint) public allowance;
     
     function addAllowance(address who, uint amount) public onlyOwner {
         allowance[who] = amount;
+    }
+    
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
     }
     
     modifier ownerOrAllowed(uint amount) {
@@ -15,14 +18,13 @@ contract SimpleWallet is Ownable {
         _;
     }
     
-    function isOwner() internal view returns(bool) {
-        return owner() == msg.sender;
-    }
-    
     function reduceAllowance(address who, uint amount) internal {
         allowance[who] -= amount;
     }
+}
 
+contract SimpleWallet is Allowance {
+    
     function withdrawMoney(address payable to, uint amount) public ownerOrAllowed(amount) {
         require(amount <= address(this).balance, "There are not enough founds stored in the smart contract");
         if(!isOwner()) {
