deec94f4bbd127dd0cb31cbc1c8048cb16692995
djaciel <dj_roses@hotmail.com>
2021-04-29 23:33:11 -0500

    re using openzeppelin smart contract

--- course/SimpleWallet/SimpleWallet.sol
+++ course/SimpleWallet/SimpleWallet.sol
@@ -1,18 +1,13 @@
 pragma solidity ^0.8.3;
 
-contract SimpleWallet {
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
+
+contract SimpleWallet is Ownable {
     
-    address owner;
-    
-    constructor() {
-        owner = msg.sender;
+    function isOwner() internal view returns(bool) {
+        return owner() == msg.sender;
     }
-    
-    modifier onlyOwner() {
-        require(owner == msg.sender, "You are not allowed");
-        _;
-    }
-    
+
     function withdrawMoney(address payable to, uint amount) public onlyOwner {
         
         to.transfer(amount);
