3a642757abdb3e9a4255b86c872470b140c55a84
djaciel <dj_roses@hotmail.com>
2021-04-29 23:47:51 -0500

    add allowance functionality

--- course/SimpleWallet/SimpleWallet.sol
+++ course/SimpleWallet/SimpleWallet.sol
@@ -4,12 +4,22 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 contract SimpleWallet is Ownable {
     
+    mapping(address => uint) public allowance;
+    
+    function addAllowance(address who, uint amount) public onlyOwner {
+        allowance[who] = amount;
+    }
+    
+    modifier ownerOrAllowed(uint amount) {
+        require(isOwner() || allowance[msg.sender] >= amount, "You are not allowed");
+        _;
+    }
+    
     function isOwner() internal view returns(bool) {
         return owner() == msg.sender;
     }
 
-    function withdrawMoney(address payable to, uint amount) public onlyOwner {
-        
+    function withdrawMoney(address payable to, uint amount) public ownerOrAllowed(amount) {
         to.transfer(amount);
     }
     
