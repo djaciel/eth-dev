7546de5b77dab486e17093ae17cf664b29f32f9c
djaciel <dj_roses@hotmail.com>
2021-05-01 16:52:25 -0500

    add web3 browser

---
+++ web3/example3.js
@@ -0,0 +1,54 @@
+const Web3 = require('web3');
+
+const web3 = new Web3(new Web3.providers.HttpProvider('http://localhost:7545'));
+
+web3.eth.getAccounts().then((res) => console.log(res));
+
+const contract = new web3.eth.Contract(
+  [
+    {
+      inputs: [
+        {
+          internalType: 'uint256',
+          name: '_myUint',
+          type: 'uint256',
+        },
+      ],
+      name: 'setUint',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'myUint',
+      outputs: [
+        {
+          internalType: 'uint256',
+          name: '',
+          type: 'uint256',
+        },
+      ],
+      stateMutability: 'view',
+      type: 'function',
+    },
+  ],
+  '0x1f36367BDf8E26d7472D359338c7e5340Da737cc'
+);
+
+contract.methods
+  .myUint()
+  .call()
+  .then((res) => console.log(res));
+
+contract.methods
+  .setUint(519)
+  .send({
+    from: '0xA8E676EB0B5B7d52f35E41b85f2a54507aE12dfB',
+  })
+  .then((res) => console.log(res));
+
+contract.methods
+  .myUint()
+  .call()
+  .then((res) => console.log(res));
